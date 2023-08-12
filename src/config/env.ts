c7919b8515b0f4bdd016d1ce326050db22c19ace
growTecsVedang <growtecs.vedang@gmail.com>
2023-08-12 21:56:59 +0530

    first commit

---
+++ src/config/env.ts
@@ -0,0 +1,45 @@
+import { str, envsafe, port, url } from "envsafe";
+
+export const env = envsafe({
+  PORT: port({
+    devDefault: 4501,
+    desc: "The port the app is running on",
+    example: 80,
+  }),
+  NODE_ENV: str({
+    devDefault: "development",
+    choices: ["development", "production"],
+  }),
+
+  // endpoint
+  ENDPOINT: url(),
+
+  // google auth
+  GOOGLE_CLIENT_ID: str(),
+  GOOGLE_CLIENT_SECRET: str(),
+
+  // facebook auth
+  FACEBOOK_CLIENT_ID: str(),
+  FACEBOOK_CLIENT_SECRET: str(),
+
+  // database
+  DATABASE_DEV_URI: url(),
+  DATABASE_PROD_URI: url(),
+
+  // access token
+  ACCESS_TOKEN_PRIVATE: str(),
+  ACCESS_TOKEN_PUBLIC: str(),
+
+  // refresh token
+  REFRESH_TOKEN_PRIVATE: str(),
+  REFRESH_TOKEN_PUBLIC: str(),
+
+  // email token
+  EMAIL_TOKEN_PRIVATE: str(),
+  EMAIL_TOKEN_PUBLIC: str(),
+
+  // Role Secret
+  USER_ROLE: str(),
+  ADMIN_ROLE: str(),
+  SUPERADMIN_ROLE: str(),
+});
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import * as dotenv from "dotenv";
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
+import express, { Application, Request, Response } from "express";
+import { env } from "./config/env";
+const app: Application = express();
+const PORT = Number(env.PORT) || 4502;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", function (req: Request, res: Response) {
+  res.send("Hi");
+  // res.status(200).json({ msg: "Noice" });
+});
+
+app.listen(PORT, async () => {
+  console.log(`server running on port ${PORT}, in ${env.NODE_ENV} enviroment`);
+});
+
+export default app;
